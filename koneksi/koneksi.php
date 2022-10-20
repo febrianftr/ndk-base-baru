52ba2355a27715588649b892b2b6d4c4e69b28ac
Andika Utama <32920246+kutamz123@users.noreply.github.com>
2022-10-20 09:29:25 +0700

    delete koneksi

--- koneksi/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-$database_ris = 'intimedika_base';
-$database_pacsio = 'pacsio';
-$database_mppsio = 'mppsio';
-$conn = mysqli_connect("localhost", "root", "efotoadmin", $database_ris);
-$conn_mppsio = mysqli_connect("localhost", "root", "efotoadmin", $database_mppsio);
-$conn_pacsio = mysqli_connect("localhost", "root", "efotoadmin", $database_pacsio);
