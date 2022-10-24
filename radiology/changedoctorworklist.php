18ec26a8b256416596085fd3202e229b8679a282
kutamz123 <andikautama034@gmail.com>
2022-10-24 15:25:53 +0700

    remake changedoctorworklist.php 50%

--- radiology/changedoctorworklist.php
+++ radiology/changedoctorworklist.php
@@ -1,43 +1,35 @@
 <?php
 
 require 'function_radiology.php';
+require '../model/query-base-dokter-radiology.php';
 
 session_start();
 
 $uid = $_GET['uid'];
 
-$query1 = mysqli_query($conn, "SELECT * FROM xray_exam2 WHERE uid = '$uid' ");
-
-$row1 = mysqli_fetch_assoc($query1);
-
-$dokradid1 = $row1['dokradid'];
-
-$query = mysqli_query($conn, "SELECT * FROM xray_dokter_radiology WHERE NOT dokradid = '$dokradid1' AND NOT dokradid = '5' ");
-$count = mysqli_num_rows($query);
+$query = mysqli_query(
+	$conn,
+	"SELECT $select_dokter_radiology 
+	FROM $table_dokter_radiology"
+);
 
 if (isset($_POST["submit"])) {
 	if (ubahdokterworklist($_POST)) {
 		echo "
-<script>
-	alert('Data berhasil dikirimkan');
-	document.location.href= 'dicom.php';
-</script>
-";
+			<script>
+				alert('Data berhasil dikirimkan');
+				document.location.href= 'dicom.php';
+			</script>";
 	} else {
 		echo "
-<script>
-	alert('data gagal dikirimkan');
-	document.location.href= 'changedoctorworklist.php?dokradid=$dokradid';
-</script>";
+			<script>
+				alert('data gagal dikirimkan');
+				document.location.href= 'changedoctorworklist.php?dokradid=$dokradid';
+			</script>";
 	}
 }
-// if (isset($_POST['submit'])) {
-// 	echo $_POST['dokradid'];
-// 	echo $_POST['uid'];
-// }
 
-
-if ($_SESSION['level'] == "radiology") {
+if ($_SESSION['level'] == "radiology" || $_SESSION['level'] == "radiographer") {
 ?>
 	<!DOCTYPE html>
 	<html lang="en">
@@ -53,28 +45,25 @@ if ($_SESSION['level'] == "radiology") {
 		<?php include('sidebar.php'); ?>
 		<div class="container" id="main">
 			<div class="row">
-
 				<div id="content1">
 					<div class="container-fluid">
 						<div class="about-inti col-md-6 col-md-offset-3" style="background-color: #f2f2f2;padding: 10px 85px;">
-
 							<form action="" method="post">
 								<?php while ($row = mysqli_fetch_assoc($query)) { ?>
 									<input type="hidden" name="uid" value="<?= $uid ?>">
 									<div class="custom-control custom-radio">
 										<input type="radio" class="custom-control-input" id="<?php echo $row['dokradid'] ?>" name="dokradid" value="<?= $row['dokradid'] ?>" required>
-										<label class="custom-control-label" for="<?php echo $row['dokradid'] ?>">&nbsp;<h3><?php echo $row['dokrad_name'] . '' . $row['dokrad_lastname'] ?> </h3></label>
+										<label class="custom-control-label" for="<?php echo $row['dokradid'] ?>">
+											&nbsp;
+											<h3><?= ucwords($row['dokrad_fullname']); ?> </h3>
+										</label>
 									</div>
 								<?php } ?>
-
 								<input type="submit" class="btn btn-primary btn-lg" value="Pilih" name="submit">
 						</div>
 						</form>
-
 					</div>
 				</div>
-
-
 			</div>
 		</div>
 		<div class="footerindex">
@@ -83,7 +72,6 @@ if ($_SESSION['level'] == "radiology") {
 			</div>
 		</div>
 		<?php include('script-footer.php'); ?>
-
 	</body>
 
 	</html>
