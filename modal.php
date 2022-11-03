e276e0c76bd1a8eb7d9aeceab6cf6cd4e82d9f0e
febrianftr <skylinesxfebrian@gmail.com>
2022-11-03 09:25:59 +0700

    update

--- modal.php
+++ modal.php
@@ -1,6 +1,6 @@
 <!-- pop up hasil semua (klik nama) -->
 <div class="modal" id="modal-all">
-    <div class="modal-dialog modal-dialog-scrollable">
+    <div class="modal-dialog modal-dialog-scrollable modal-lg">
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal">×</button>
