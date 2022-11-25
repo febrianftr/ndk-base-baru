29c402a1af4eadb2edf48d3bbdbf3e89b2ca09ab
kutamz123 <andikautama034@gmail.com>
2022-11-25 13:43:12 +0700

    mengubah ckeditor config.js di worklist.php untuk mengatur width sesuai A4

--- radiology/head.php
+++ radiology/head.php
@@ -12,7 +12,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" type="text/css" media="screen" href="css/jquery.datetimepicker.min.css">
 <link href="../css/select2.min.css" rel="stylesheet" />
-<link rel="stylesheet" type="text/css" href="../css/style_master.css" />
+<link rel="stylesheet" type="text/css" href="../css/style_master.css?v=3" />
 <link rel="stylesheet" href="css/css_media.css">
 
 <!-- untuk validasi form -->
