25818a1e852471eb22df47f1538f6c44f997d3b3
kutamz123 <andikautama034@gmail.com>
2023-04-10 10:55:07 +0700

    hasil template merge

--- hasil-template.php
+++ hasil-template.php
@@ -10,30 +10,10 @@ $row = mysqli_fetch_assoc(mysqli_query(
 
 <div class="fill">
     <h6 class="text-center font-weight-bold">
-        <label><?= $row['title']; ?></label><button onclick="copyToClipboard('#p1')">Copy</button>
+        <label><?= $row['title']; ?></label>
     </h6>
-<<<<<<< HEAD
-    <p id="p1">
-        <?= $row['fill']; ?>
-    </p>
-</div>
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script>
-function copyToClipboard(element) {
-  var $temp = $("<input>");
-  $("body").append($temp);
-  $temp.val($(element).text()).select();
-  document.execCommand("copy");
-  $temp.remove();
-}
-</script>
-=======
     <!-- <button onclick="selectText('divid')">Select</button> -->
     <div id="divid">
         <?= $row['fill']; ?>
     </div>
-
-
 </div>
->>>>>>> e7063c3bc7596b52f4177d7a027b716f7da09384
